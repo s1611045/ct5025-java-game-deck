6766074a9aa0abfda0d55403574bc9f7cfcea280
s1611045 <s1611045@connect.glos.ac.uk>
2017-11-29 13:10:55 +0000

    Add functionality to noughts and crosses game

--- src/uk/ac/glos/ct5025/games/NoughtsGame.java
+++ src/uk/ac/glos/ct5025/games/NoughtsGame.java
@@ -8,9 +8,99 @@ public class NoughtsGame {
     private Object player2;
     private char player1Symbol = 'o';
     private char player2Symbol = 'x';
+    private char currentPlayerSymbol = 'o';
+    private long startTime = System.currentTimeMillis();
+
+    private int[] askHumanForMove() {
+        getRow();
+        //return statement
+    }
+
+    private int getRow() {
+        boolean isValidInput = false;
+        char input;
+        int row = 0;
+
+        while (!isValidInput) {
+            System.out.print("Enter the row: ");
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            String inputString = scanner.next();
+            input = inputString.charAt(0);
+            if (!Character.isDigit(input)) {
+                System.out.print("Invalid input! Try again.\n");
+                isValidInput = false;
+            }
+            else if (!(input == '1') || !(input == '2') || !(input == '3')) {
+                System.out.print("Invalid input! Try again.\n");
+                isValidInput = false;
+            }
+            else {
+                row = Integer.parseInt(Character.toString(input));
+                row--;
+                isValidInput = true;
+            }
+        }
+        return row;
+    }
+
 
     private void addSymbolToBoard(int row, int column, char symbol) {
         this.gameBoard[row][column] = symbol;
+    }
+
+    private boolean checkSquareContainsSymbol(int row, int column) {
+        if ((this.gameBoard[row][column] == 'o') || (this.gameBoard[row][column] == 'x')) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void changeCurrentPlayer() {
+        if (this.currentPlayerSymbol == 'o') {
+            this.currentPlayerSymbol = 'x';
+        }
+        else {
+            this.currentPlayerSymbol = 'o';
+        }
+    }
+
+    private void printGameBoard() {
+        //Print numbers identifying board columns
+        System.out.print("  1  2  3");
+        int rowNumber = 1;
+        //Traverse board
+        for (int i=0;i<BOARD_DIMENSION;i++) {
+            //Print and increment row number
+            System.out.print(rowNumber);
+            rowNumber++;
+            for (int e=0;e<BOARD_DIMENSION;e++) {
+                //If board square is empty, print empty character
+                //If not, print symbol
+                if (this.gameBoard[i][e] == ' ') {
+                    System.out.print("[ ]");
+                }
+                else {
+                    System.out.print("[" + this.gameBoard[i][e] + "]");
+                }
+            }
+            //New line after row is printed
+            System.out.print("\n");
+        }
+    }
+
+    private void computerMakeMove() {
+        boolean moveMade = false;
+        java.util.Random rand = new java.util.Random();
+        while (!moveMade) {
+            int row = (rand.nextInt(3));
+            int column = (rand.nextInt(3));
+            if (!checkSquareContainsSymbol(row, column)) {
+                addSymbolToBoard(row, column, this.currentPlayerSymbol);
+                moveMade = true;
+            }
+        }
     }
 
     private boolean checkColumns() {
@@ -46,7 +136,7 @@ public class NoughtsGame {
     }
 
     private boolean checkWinner() {
-        return this.checkRows() || this.checkColumns() || this.checkDiags();
+        return (this.checkRows() || this.checkColumns() || this.checkDiags());
     }
 
 
--- src/uk/ac/glos/ct5025/scorecards/NoughtsGameScorecard.java
+++ src/uk/ac/glos/ct5025/scorecards/NoughtsGameScorecard.java
@@ -1,32 +1,13 @@
 package uk.ac.glos.ct5025.scorecards;
 
 public class NoughtsGameScorecard extends Scorecard {
-    private int numberOfRounds;
-    private int numberOfTies;
-    private char winningSymbol;
+    private int numberOfTurns;
 
-    //Encapsulate fields
-    public int getNumberOfRounds() {
-        return numberOfRounds;
+    public int getNumberOfTurns() {
+        return numberOfTurns;
     }
 
-    public void setNumberOfRounds(int numberOfRounds) {
-        this.numberOfRounds = numberOfRounds;
-    }
-
-    public int getNumberOfTies() {
-        return numberOfTies;
-    }
-
-    public void setNumberOfTies(int numberOfTies) {
-        this.numberOfTies = numberOfTies;
-    }
-
-    public char getWinningSymbol() {
-        return winningSymbol;
-    }
-
-    public void setWinningSymbol(char winningSymbol) {
-        this.winningSymbol = winningSymbol;
+    public void setNumberOfTurns(int numberOfTurns) {
+        this.numberOfTurns = numberOfTurns;
     }
 }
